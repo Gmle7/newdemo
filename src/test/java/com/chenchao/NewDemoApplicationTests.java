b1f7f6e2c749e3f8a54815a8baab41bc5e69053f
Gmle7 <cc19951117@live.com>
2018-01-13 17:36:18 +0800

    测试类改正，pom文件取消oracle数据库驱动

--- src/test/java/com/chenchao/NewDemoApplicationTests.java
+++ src/test/java/com/chenchao/NewDemoApplicationTests.java
@@ -28,7 +28,7 @@ public class NewDemoApplicationTests {
 
 	@Before
 	public void setUp() throws Exception{
-		this.base = new URL("http://localhost:" + port + "/");
+		this.base = new URL("http://localhost:" + port + "/hello");
 	}
 	@Test
 	public void getHello() throws Exception{
